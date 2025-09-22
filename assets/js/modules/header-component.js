7abf513985c199dc61ce3840a04c9f587989a398
Anna Stein <anna.stein@smail.th-koeln.de>
2025-09-22 22:39:39 +0200

    F15

---
+++ assets/js/modules/header-component.js
@@ -0,0 +1,16 @@
+class MyHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+      <header class="header">
+        <div class="home-button">
+          <a href="/"><i class="icofont-book-alt is-large"></i></a>
+        </div>
+        <h1 class="title">Praxisprojekte & Abschlussarbeiten</h1>
+        <h2 class="subtitle">bei <a href="https://christiannoss.de">Christian Noss</a></h2>
+      </header>
+    `;
+  }
+}
+
+customElements.define("my-header", MyHeader);
